1d63139454021f6e2f2db0606f697c5915678ea1
H3NZPLAYZ <c.hugo.g.maldonado@gmail.com>
2026-04-23 15:59:34 +0200

    Chapter 68

--- polling_demo/sports/views.py
+++ polling_demo/sports/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from sports.models import Fixture
 
 # Create your views here.
+def fixtures(request):
+    fixtures = Fixture.objects.all()
+    context = {
+        'fixtures': fixtures,
+    }
+    return render(request, 'sports/fixtures.html', context)
